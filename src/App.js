fcd46c6cb623e375a800d6932daf490cea3dcb77
Yousuf <s.yousuf@live.com>
2019-08-06 10:03:06 +0500

    React Router added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component, Fragment } from "react";
-import Header from "./components/Header/Header";
+// import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Body from "./components/Body/Body";
+// import Body from "./components/Body/Body";
+import Router from "./components/routes/Router";
 import "./App.css";
 
 class App extends Component {
@@ -18,8 +19,9 @@ class App extends Component {
   render() {
     return (
       <Fragment>
-        <Header changeState={this.changeState} />
-        <Body st={this.state.pageVisited} />
+        {/* <Header changeState={this.changeState} />
+        <Body st={this.state.pageVisited} /> */}
+        <Router />
         <div style={{ position: "relative" }}>
           <Footer />
         </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,46 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function Header(props) {
-  const nav = ["Home", "About", "Contact"];
-  return (
-    <header>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-3">
-        <div className="navbar-brand">
-          <h3>CreativO</h3>
-        </div>
+class Header extends React.Component {
+  state = {
+    links: [
+      {
+        title: "Home",
+        path: "/"
+      },
+      {
+        title: "About",
+        path: "/about"
+      },
+      {
+        title: "Contact",
+        path: "/contact"
+      }
+    ]
+  };
 
-        <ul className="nav justify-content-end text-white">
-          {nav.map((item, index) => (
-            <li key={index} className="nav-item">
-              <a className="nav-link" href="#" onClick={props.changeState}>
-                {item}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
-  );
+  render() {
+    const { links } = this.state;
+    return (
+      <header>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-3">
+          <div className="navbar-brand">
+            <h3>CreativO</h3>
+          </div>
+          }
+          <ul className="nav justify-content-end text-white">
+            {links.map((item, index) => (
+              <li key={index} className="nav-item">
+                <Link to={item.path} className="nav-link">
+                  {item.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+    );
+  }
 }
 
 export default Header;
